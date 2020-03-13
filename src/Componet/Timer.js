99c20aa4b1f304b39a82604fccb275592d4be474
Oumaima456 <oumaymas456@gmail.com>
2020-03-13 11:30:19 +0100

    test

---
+++ src/Componet/Timer.js
@@ -0,0 +1,110 @@
+import React from 'react'
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { seconds: 0, minutes: 0, hours: 0, status: 'stop' }
+
+
+        setInterval(() => { if (this.state.status !== 'stop') 
+        
+        
+        
+        { this.setState({ seconds: this.state.seconds += 1, }) } }, 1000)
+
+        setInterval(
+            () => {
+                if (this.state.status !== 'stop') {
+                    this.setState({
+
+                        minutes: this.state.minutes + 1,
+                        seconds: 0
+
+                    })
+
+                }
+            },
+
+            (1000) * 60
+
+        )
+
+
+
+        setInterval(
+            () => {
+                if (this.state.status !== 'stop') {
+                    this.setState({
+
+                        hours: this.state.hours + 1,
+                        minutes: 0
+
+                    })
+                }
+
+
+            },
+
+            ((1000) * 60) * 60
+
+        )
+
+    }
+
+    startwatch = () => {
+        if (this.state.status === 'stop') {
+            this.setState({
+                status: 'start'
+            })
+        } else {
+            {
+                this.setState({
+                    status: 'stop'
+                })
+            }
+        }
+    }
+
+
+    stopwatch = () => {
+        this.setState(
+            {
+                seconds: 0,
+                minutes: 0,
+                hours: 0,
+                status: 'stop'
+            }
+        )
+    }
+
+
+
+    render() {
+
+        return (
+
+            <div className='element'>
+                <div className="time">
+                    <h1>{String(this.state.hours).padStart(2, '0') + ':'}</h1>
+                    <h1>{String(this.state.minutes).padStart(2, '0') + ':'}</h1>
+                    <h1>{String(this.state.seconds).padStart(2, '0')}</h1>
+
+                </div>
+
+              
+                    <button className='btn' onClick={this.startwatch}>{this.state.status === 'stop' ? 'start' : 'pause'}</button>
+                    <button className='btn' onClick={this.stopwatch}>Reset</button>
+                
+            </div>
+        );
+
+
+    }
+
+
+
+
+}
+
+
+export default Timer;
